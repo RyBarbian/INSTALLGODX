ed7b562e80302367d2d10a1f131ac9c07edff333
Ubuntu <ubuntu@ip-172-31-24-89.us-east-2.compute.internal>
2018-03-06 03:55:27 +0000

    Install

---
+++ install.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+
+sudo apt-add-repository ppa:bitcoin/bitcoin
+
+
+
+#//Make sure build enviroment is up to date
+
+sudo apt-get update 
+sudo apt-get install git build-essential g++ libssl-dev libboost-all-dev libdb4.8++-dev libqrencode-dev qt4-qmake libqt4-dev
+
+
+#//Download and Install Mini-UPnPC
+
+mkdir miniupnpc
+cd miniupnpc
+wget "http://miniupnp.tuxfamily.org/files/miniupnpc-1.9.tar.gz"
+tar -zxvf miniupnpc-1.9.tar.gz
+cd miniupnpc-1.9/
+sudo make install
+cd ~/
+
+
+#//Clone the GoDxoin Beta v 1.0.1 repo from Git
+
+git clone https://github.com/RyBarbian/noixDoGB1.0.git
+#//(it's spelled backward and dl's a folder named noixDoGB1.0/src to keep it semi-secret (just to keep the test net secure))
+
+
+
+#Compile the GoDxoin Beta v 1.0.1 daemon (godxoind)
+
+cd ~/noixDoGB1.0/src/
+make -f makefile.unix
+
+# daemon will be compiled, and can be started from the command line
+#($ .godxoind --daemon) 
+
+cd ~/noixDoGB1.0
+qmake
+rm Makefile
+git clone https://github.com/RyBarbian/GoDxoinMakefile.git
+make 
+cd ~/
+
+#qt wallet will be compiled, and can be run by double clicking godxoin executable
+
+
+#In the $ .godxoin folder make a godxoin.conf file
+
+mkdir ~/.godxoin
+cd .godxoin
+touch godxoin.conf
+cd ..
+cd ~/noixDoGB1.0/src/
+./godxoind --daemon
+
+
