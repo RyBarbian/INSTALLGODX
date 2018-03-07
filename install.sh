8e327ec34a1cdf131886f78e4a7a400ffcf8d4fd
RyBarbian <36339857+RyBarbian@users.noreply.github.com>
2018-03-06 16:23:05 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,40 +24,42 @@ cd ~/
 
 #//Clone the GoDxoin Beta v 1.0.1 repo from Git
 
-git clone https://github.com/RyBarbian/noixDoGB1.0.git
-#//(it's spelled backward and dl's a folder named noixDoGB1.0/src to keep it semi-secret (just to keep the test net secure))
+git clone https://github.com/RyBarbian/GoDxoinB1.1
 
 
 
 #Compile the GoDxoin Beta v 1.0.1 daemon (godxoind)
 
-cd ~/noixDoGB1.0/src/
+cd ~/GoDxoinB1.1/src/
+make clean -f makefile.unix
 make -f makefile.unix
+
+#In the $ .godxoin folder make a godxoin.conf file
+
+cd
+mkdir ~/.godxoin
+cd INSTALLGODX
+mv godxoin.conf ~/.godxoin/godxoin.conf
+cd
+
 
 # daemon will be compiled, and can be started from the command line
 #($ .godxoind --daemon) 
 
-cd ~/noixDoGB1.0
+cd ~/GoDxoinB1.1
 qmake
-rm Makefile
 git clone https://github.com/RyBarbian/GoDxoinMakefile.git
 cd GoDxoinMakefile
-cp Makefile ~/noixDoGB1.0/Makefile
-cd ~/noixDoGB1.0
+cp Makefile ~/GoDxoinB1.1/Makefile
+cd ~/GoDxoinB1.1
 make 
-cd ~/
+cd GoDxoinB1.1
+./GODXOIN-QT.PRO
 
 #qt wallet will be compiled, and can be run by double clicking godxoin executable
 
 
-#In the $ .godxoin folder make a godxoin.conf file
 
-mkdir ~/.godxoin
-cd INSTALLGODX
-mv godxoin.conf ~/.godxoin/godxoin.conf
-cd ..
-cd noixDoGB1.0
-./GODXOIN-QT.PRO
 
 
 
