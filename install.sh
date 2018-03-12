9c2899aa5ec96f08c5caa3b36d6c31fa0b0ab611
RyBarbian <36339857+RyBarbian@users.noreply.github.com>
2018-03-11 18:39:54 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ cd
 #($ .godxoind --daemon) 
 
 cd ~/noixDoGB1.0
-qmake
+qmake "USE_QRCODE=1" "USE_UPNP=1" "USE_IPV6=1" GodCoin.pro
 git clone https://github.com/RyBarbian/GoDxoinMakefile.git
 cd GoDxoinMakefile
 cp Makefile ~/noixDoGB1.0/Makefile
