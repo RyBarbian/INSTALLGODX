058c58921621903613c38f2e22b594161f1f13c1
RyBarbian <36339857+RyBarbian@users.noreply.github.com>
2018-04-08 21:21:36 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,12 +24,12 @@ cd ~/
 
 #//Clone the GoDxoin Beta v 1.0.1 repo from Git
 
-git clone https://github.com/RyBarbian/GoDxoinB1.1
+git clone https://github.com/RyBarbian/GoDxoinB1.1.1
 .git
 
 #Compile the GoDxoin Beta v 1.0.1 daemon (godxoind)
 
-cd ~/GoDxoinB1.1/src/
+cd ~/GoDxoinB1.1.1/src/
 make clean -f makefile.unix
 make -f makefile.unix
 
@@ -45,14 +45,14 @@ cd
 # daemon will be compiled, and can be started from the command line
 #($ .godxoind --daemon) 
 
-cd ~/GoDxoinB1.1
+cd ~/GoDxoinB1.1.1
 qmake "USE_UPNP=1" "USE_IPV6=1" GodCoin.pro
 git clone https://github.com/RyBarbian/GoDxoinMakefile.git
 cd GoDxoinMakefile
-cp Makefile ~/GoDxoinB1.1/Makefile
-cd ~/GoDxoinB1.1
+cp Makefile ~/GoDxoinB1.1.1/Makefile
+cd ~/GoDxoinB1.1.1
 make 
-cd GoDxoinB1.1
+cd GoDxoinB1.1.1
 ./GODXOIN-QT.PRO
 
 #qt wallet will be compiled, and can be run by double clicking godxoin executable
